b02e3f4245c0cd671b7a6c562089e02ad62a738b
andres.mera <feli041089@gmail.com>
2024-03-26 10:46:59 -0500

    developer : De observable a Señal - toSignal

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TitleComponent } from '@shared/title/title.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { User } from '../../../interfaces/req-response';
+import { switchMap } from 'rxjs';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './user.component.html',
-  styles: ``
 })
 export default class UserComponent {
 
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
+
+  // public user = signal<User | undefined>(undefined);
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  );
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { User, UsersResponse } from '../interfaces/req-response';
+import type { User, UserResponse, UsersResponse } from '../interfaces/req-response';
 import { HttpClient } from '@angular/common/http';
-import { delay } from 'rxjs';
+import { delay, map } from 'rxjs';
 
 interface State {
   users: User[];
@@ -33,4 +33,11 @@ export class UsersService {
       });
   }
 
+  getUserById(id: string) {
+    return this.http.get<UserResponse>(`https://reqres.in/api/users/${id}`)
+      .pipe(
+        delay(1500),
+        map(resp => resp.data)
+      );
+  }
 }
