08bf1511a27420686ed21bf58401c7a6e50fdd81
andres.mera <feli041089@gmail.com>
2024-03-26 10:53:11 -0500

    developer : Tarea - Computed Signals.

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TitleComponent } from '@shared/title/title.component';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -17,10 +17,17 @@ export default class UserComponent {
   private route = inject(ActivatedRoute);
   private usersService = inject(UsersService);
 
+
   // public user = signal<User | undefined>(undefined);
   public user = toSignal(
     this.route.params.pipe(
       switchMap(({ id }) => this.usersService.getUserById(id))
     )
   );
+
+  public titleLabel = computed(() => {
+    if (this.user())
+      return `Informacion del usuario: ${this.user()?.first_name} ${this.user()?.last_name}`;
+    return 'Informacion del usuario';
+  });
 }
