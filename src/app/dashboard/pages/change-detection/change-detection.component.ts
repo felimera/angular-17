9784e4d15bc6ecb8b1e2cd3678921a5a19937059
andres.mera <feli041089@gmail.com>
2024-03-26 08:56:08 -0500

    developer : ChangeDetection - OnPush.

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-title title="Change Detection"/>
+    <pre>{{frameworkAsSignal() | json}}</pre>
+    <pre>{{frameworkAsProperty | json}}</pre>
+  `
 })
 export default class ChangeDetectionComponent {
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: '2016',
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: '2016',
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update(value => ({
+        ...value,
+        name: 'React',
+      }));
+      console.log('Hecho.');
+    }, 3000);
+  }
 }
