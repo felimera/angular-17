b36729502769ecc464cee2e9dbd52106471d1989
andres.mera <feli041089@gmail.com>
2024-03-26 08:36:33 -0500

    developer : Requerid @Input

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 type Grade = 'A' | 'B' | 'F';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
@@ -12,8 +13,8 @@ export default class ControlFlowComponent {
 
   public showContent = signal(false);
   public grade = signal<Grade>('A');
-  public frameworks=signal(['Angular','Vue','Svelte','Qwik','React']);
-  public frameworks2=signal([]);
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik', 'React']);
+  public frameworks2 = signal([]);
 
   public toggleContent(): void {
     this.showContent.update(value => !value);
