2aaa755647c7f99a5e0d5c39e851fd78b63fa5a5
andres.mera <feli041089@gmail.com>
2024-03-26 10:00:18 -0500

    developer : Servicios con señales.

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   standalone: true,
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export default class UsersComponent {
 
+  public usersService = inject(UsersService);
 }
