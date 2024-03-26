6f395ae7285b777b5a9315922b6eac258bd153b3
andres.mera <feli041089@gmail.com>
2024-03-26 10:22:10 -0500

    developer : Peticiones Http - ImportProvidersFrom.

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { UsersService } from '../../../services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
